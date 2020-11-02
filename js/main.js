0bce5e8199dfb987907b15b08787e7beee81d7d0
Phuc <sucanabo@gmail.com>
2020-11-02 09:05:18 +0700

    edit 2.11

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
-$(document).ready(function () {
-  $("#annouce-btn").click(function () {
-    $(".ann-popover").toggleClass("d-block")
+// $(document).ready(function () {
+//   $("#annouce-btn").click(function () {
+//     $(".ann-popover").toggleClass("d-block")
+//   });
+//   $("#noti-btn").click(function () {
+//     $(".noti-popover").toggleClass("d-block")
+//   });
+//   $("#write-btn").click(function () {
+//     $("#write-dropdown").toggleClass("d-block")
+//   });
+//   $("#user-setting-btn").click(function () {
+//     $(".user-menu__popover").toggleClass("d-block")
+//   });
+// });
+ var navController = document.querySelectorAll('.nav-control');
+ var popovers = document.querySelectorAll('.popover');
+for(var i = 0; i < navController.length; ++i){
+  navController[i].addEventListener('click',function(e){
+    console.log(popovers[i]);
   });
-  $("#noti-btn").click(function () {
-    $(".noti-popover").toggleClass("d-block")
-  });
-  $("#write-btn").click(function () {
-    $("#write-dropdown").toggleClass("d-block")
-  });
-  $("#user-setting-btn").click(function () {
-    $(".user-menu__popover").toggleClass("d-block")
-  });
-});
+}
